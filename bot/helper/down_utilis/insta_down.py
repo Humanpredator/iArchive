a97f3d5a3ce346425e0ecd79c37aa236305829d5
Humanpredator <teamplague2022@gmail.com>
2022-08-06 08:14:33 +0530

    Some fixes

--- bot/helper/down_utilis/insta_down.py
+++ bot/helper/down_utilis/insta_down.py
@@ -1,26 +1,25 @@
-import shutil
 import pytz
 import subprocess
 import time
-from bot.helper.ext_utils.fs_utils import subfolder
+from bot.helper.ext_utils.fs_utils import clean_download, subfolder
 from bot.helper.upload_utilis.gdrive import gup
 from bot.helper.telegram_helper.message_utils import *
 from datetime import datetime
 import threading
 from bot.helper.ext_utils.bot_utils import usercheck
-from bot import DOWNLOAD_STATUS_UPDATE_INTERVAL,TG_UPLOAD
+from bot import DOWNLOAD_STATUS_UPDATE_INTERVAL, TG_UPLOAD
 from bot.helper.upload_utilis.tg_upload import tgup
 IST = pytz.timezone('Asia/Kolkata')
 
 
-#A function to download content from Instagram
-def download_insta(command, m,dir,username,chat_id,fetch):
-    def download(command,m,dir,fetch):
+# A function to download content from Instagram
+def download_insta(command, m, dir, username, chat_id, fetch):
+    def download(command, m, dir, fetch):
         USER = usercheck()
-        session=f"./{USER}"
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        session = f"./{USER}"
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
         while True:
-            time.sleep(0.5)
             output = process.stdout.readline()
             if output == b'':
                 print("Finished Output")
@@ -29,15 +28,15 @@ def download_insta(command, m,dir,username,chat_id,fetch):
                 datetime_ist = datetime.now(IST)
                 ISTIME = datetime_ist.strftime("%I:%M:%S %p - %d %B %Y")
                 try:
-                    msg=f'CURRENT_STATUS ⚙️ : <code>{format(output.decode("UTF8"))}</code>\n<b>Last Updated</b> : <code>{ISTIME}</code>\n<b>Directory</b> : <code>{dir}</code>\n<b>session</b> : <code>{session}</code>\n<b>Type</b> : <code>{fetch}</code>'
+                    msg = f'CURRENT_STATUS ⚙️ : <code>{format(output.decode("UTF8"))}</code>\n<b>Last Updated</b> : <code>{ISTIME}</code>\n<b>Directory</b> : <code>{dir}</code>\n<b>session</b> : <code>{session}</code>\n<b>Type</b> : <code>{fetch}</code>'
                     time.sleep(DOWNLOAD_STATUS_UPDATE_INTERVAL)
-                    bot.edit_message_text(msg, m.chat.id, m.message_id, parse_mode="HTML")
+                    bot.edit_message_text(
+                        msg, m.chat.id, m.message_id, parse_mode="HTML")
                     LOGGER.info(f"{output.decode('UTF8')}")
                 except Exception as e:
                     LOGGER.info(f"{e}")
                     pass
         while True:
-            time.sleep(0.5)
             error = process.stderr.readline()
             if error == b'':
                 print("Finished No error")
@@ -46,22 +45,24 @@ def download_insta(command, m,dir,username,chat_id,fetch):
                 datetime_ist = datetime.now(IST)
                 ISTIME = datetime_ist.strftime("%I:%M:%S %p - %d %B %Y")
                 try:
-                    ermsg="ERROR ❌ : <code>{}</code>\nLast Updated : <code>{}</code>".format(error.decode("UTF8"), ISTIME)
+                    ermsg = "ERROR ❌ : <code>{}</code>\nLast Updated : <code>{}</code>".format(
+                        error.decode("UTF8"), ISTIME)
                     time.sleep(DOWNLOAD_STATUS_UPDATE_INTERVAL)
-                    bot.edit_message_text(ermsg, m.chat.id, m.message_id, parse_mode="HTML")
-                    LOGGER.info(f"{error.decode('UTF8')}")       
+                    bot.edit_message_text(
+                        ermsg, m.chat.id, m.message_id, parse_mode="HTML")
+                    LOGGER.info(f"{error.decode('UTF8')}")
                 except Exception as e:
                     LOGGER.info(f"{e}")
                     pass
                 return True
-        LOGGER.info(f"Download Complete for {dir}")
+        LOGGER.info(f"Download Completed-{dir}")
         subfolder(dir)
-        gup(dir, m,username,fetch)
+        gup(dir, m, username, fetch)
         if TG_UPLOAD:
             tgup(chat_id, dir)
         else:
             print("TG_UPLOAD is disabled")
             pass
-        shutil.rmtree(dir)    
-    threading.Thread(target=download, args=(command,m,dir,fetch)).start()
+        clean_download(dir)
+    threading.Thread(target=download, args=(command, m, dir, fetch)).start()
     return True

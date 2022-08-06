c95972bba90e23f68f8f95728d8dc8709fabce5e
Humanpredator <teamplague2022@gmail.com>
2022-08-06 08:17:28 +0530

    Formatted and some fixes

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -1,24 +1,12 @@
 import sys
 import shutil
 import os
-from bot import LOGGER,OWNER_ID
-import shutil
+from bot import LOGGER, OWNER_ID
 from pathlib import Path
 
-from bot.helper.ext_utils.bot_utils import usercheck
-
-#function to remove sub dir
-def rmv(subdir):
-      dir = Path(subdir).is_dir()
-      if dir == True:
-            shutil.rmtree(subdir)
-            print("sub folder successfully removed")
-      else:
-            print("Path doesn't exists....Skiped!")
-            pass
 
 def subpath(path):
-    subfolder=[]
+    subfolder = []
     try:
         for it in os.scandir(path):
             if it.is_dir():
@@ -27,47 +15,61 @@ def subpath(path):
     except:
         pass
 
+
+def rmv(subdir):
+    dir = Path(subdir).is_dir()
+    if dir == True:
+        shutil.rmtree(subdir)
+        print("sub folder successfully removed")
+    else:
+        print("Path doesn't exists....Skiped!")
+        pass
+
+
 def subfolder(DIR):
     try:
-        dirlist= subpath(DIR)
+        dirlist = subpath(DIR)
         for dir in dirlist:
             files = os.listdir(dir)
             for file in files:
                 file_name = os.path.join(dir, file)
-                shutil.move(file_name, DIR +'/'+file)
+                shutil.move(file_name, DIR + '/'+file)
                 print("Files Moved")
                 print(f'{DIR},{file_name}')
             rmv(dir)
     except:
-        pass  
+        pass
 
-username=usercheck()
 
-dir=f"{OWNER_ID}/{username}"
 def clean_download(path: str):
     if os.path.exists(path):
         LOGGER.info(f"Cleaning Download: {path}")
         shutil.rmtree(path)
 
+
 def start_cleanup():
     try:
+        dir = f"{OWNER_ID}"
         shutil.rmtree(dir)
     except FileNotFoundError:
         pass
+
 
 def clean_all():
     try:
+        dir = f"{OWNER_ID}"
         shutil.rmtree(dir)
     except FileNotFoundError:
         pass
 
+
 def exit_clean_up(signal, frame):
     try:
-        LOGGER.info("Please wait, while we clean up the downloads and stop running downloads")
+        LOGGER.info(
+            "Please wait, while we clean up the downloads and stop running downloads")
         clean_all()
         os.remove('username.txt')
         sys.exit(0)
     except KeyboardInterrupt:
         LOGGER.warning("Force Exiting before the cleanup finishes!")
         sys.exit(1)
-
